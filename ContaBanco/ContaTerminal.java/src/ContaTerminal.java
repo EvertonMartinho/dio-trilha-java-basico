9c25dfbe6314109f2c37b055f574d33790c5b9e6
EvertonMartinho <everton.santos@embratel.com.br>
2024-08-22 16:30:05 -0300

    Updte

--- ContaBanco/ContaTerminal.java/src/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java/src/ContaTerminal.java
@@ -1,12 +1,5 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        //TODO: conhecer e importar a class scanner
-
-        //EXIBIR as msgs para nossos usuários
-
-        //Obter pela class scanner os valores digitados no terminal
-
-
-        //Exibir a msg conta criada
+        System.out.println("Olá Brasil");
     }
 }
