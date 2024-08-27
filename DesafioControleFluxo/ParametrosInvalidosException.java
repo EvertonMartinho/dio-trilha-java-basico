00a46183813d621dc85779522a9a2a8969396d28
EvertonMartinho <everton.santos@embratel.com.br>
2024-08-27 10:37:07 -0300

    comit

---
+++ DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+// Crie este arquivo como ParametrosInvalidosException.java
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
