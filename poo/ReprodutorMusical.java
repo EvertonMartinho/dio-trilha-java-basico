38b9a950c020e31014d01198b85a1c98130e49f6
EvertonMartinho <everton.santos@embratel.com.br>
2024-08-27 10:52:06 -0300

    commit

---
+++ poo/ReprodutorMusical.java
@@ -0,0 +1,46 @@
+package poo;
+
+// Classe para o Reprodutor Musical
+public class ReprodutorMusical {
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+}
+
+// Classe para o Aparelho Telefônico
+public class AparelhoTelefonico {
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    public void atender() {
+        System.out.println("Atendendo a chamada.");
+    }
+
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+}
+
+// Classe para o Navegador na Internet
+public class NavegadorInternet {
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+}
